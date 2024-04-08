c67156d030d847c87a972faf240813c4754ef88b
Wim Tibackx <wim@sparklink.be>
2024-04-08 07:21:41 +0200

    feat: switch tooling to our SPRK defaults

---
+++ .eslintrc.cjs
@@ -0,0 +1,63 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'ignorePatterns': [
+    '**/node_modules/',
+    'dist/'
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2,
+      {
+        'SwitchCase': 1,
+      }
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'warn',
+      'single'
+    ],
+    'semi': [
+      'warn',
+      'always',
+      {
+        'omitLastInOneLineBlock': true
+      }
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
+    '@typescript-eslint/no-namespace': 'off',
+  }
+};
